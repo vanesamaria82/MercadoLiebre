d3b9a99c745bdd91de688a82c7e43961b4396f21
vanesamaria82 <vpaschetta@gmail.com>
2023-11-23 22:27:32 -0300

    corrigiendo

--- src/app.js
+++ src/app.js
@@ -5,23 +5,23 @@ const path = require("path");
 //guardar sus funcionalidades dentro de una variable
 
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "../public")));
 //utilizar el listen para levantar el servidor
 app.listen (3700, () => console.log ("Mercado Liebre")
 ) 
 
 app.get("/", (req, res)=> 
-    res.sendFile (path.join(__dirname, "./src/views/index.html"))
+    res.sendFile (path.join(__dirname, "/views/index.html"))
 ) 
 
 app.get("/register", (req, res)=> 
-    res.sendFile (path.join(__dirname, "./src/views/register.html"))
+    res.sendFile (path.join(__dirname, "/views/register.html"))
 )
 
 app.get("/login", (req, res)=> 
-    res.sendFile (path.join(__dirname, "./src/views/login.html"))
+    res.sendFile (path.join(__dirname, "/views/login.html"))
 )
 
 app.post("/login", (req, res)=> 
-    res.sendFile (path.join(__dirname, "./src/views/index.html"))
+    res.sendFile (path.join(__dirname, "/views/index.html"))
 )
